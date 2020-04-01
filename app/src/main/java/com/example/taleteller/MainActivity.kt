53faee99171be5dd00ab37a897fba85d1861163a
Rafal <tellertale.us@gmail.com>
2020-04-02 00:01:26 +0200

    Login & Sign up

--- app/src/main/java/com/example/taleteller/MainActivity.kt
+++ app/src/main/java/com/example/taleteller/MainActivity.kt
@@ -1,12 +1,88 @@
 package com.example.taleteller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+
+        gotoregisterbutton.setOnClickListener(){
+            startActivity(Intent(this,Register::class.java))
+            finish()
+        }
+
+        loginbutton.setOnClickListener{
+            doLogin()
+        }
+    }
+
+    private fun doLogin(){
+        if(loginemail.text.toString().isEmpty()){
+            loginemail.error = "Please enter email"
+            loginemail.requestFocus()
+            return
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(loginemail.text.toString()).matches()){
+            loginemail.error = "Please enter email"
+            loginemail.requestFocus()
+            return
+        }
+        if(loginpassword.text.toString().isEmpty()){
+            loginpassword.error = "Please enter password"
+            loginpassword.requestFocus()
+            return
+        }
+        auth.signInWithEmailAndPassword(loginemail.text.toString(), loginpassword.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Toast.makeText(baseContext, "Login failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+
+                }
+
+            }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser : FirebaseUser?){
+        if(currentUser != null){
+            if(currentUser.isEmailVerified) {
+                startActivity(Intent(this, MainPage::class.java))
+                finish()
+            }
+            else{
+                Toast.makeText(baseContext, "Please verify your email address",
+                    Toast.LENGTH_SHORT).show()
+
+            }
+        }
+        else{
+            Toast.makeText(baseContext, "Login failed.",
+                Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/taleteller/Register.kt
@@ -0,0 +1,62 @@
+package com.example.taleteller
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
+
+class Register : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        auth = FirebaseAuth.getInstance()
+
+        registerbutton.setOnClickListener() {
+            signUpUser()
+        }
+    }
+        private fun signUpUser(){
+            if(registeremail.text.toString().isEmpty()){
+                registeremail.error = "Please enter email"
+                registeremail.requestFocus()
+                return
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(registeremail.text.toString()).matches()){
+                registeremail.error = "Please enter email"
+                registeremail.requestFocus()
+                return
+            }
+            if(registerpassword.text.toString().isEmpty()){
+                registerpassword.error = "Please enter password"
+                registerpassword.requestFocus()
+                return
+            }
+            auth.createUserWithEmailAndPassword(registeremail.text.toString(), registerpassword.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        user?.sendEmailVerification()
+                            ?.addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    startActivity(Intent(this,MainActivity::class.java))
+                                    finish()
+                                }
+                            }
+
+                    } else {
+                        Toast.makeText(baseContext, "Sing Up failed. Try again later",
+                            Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    // ...
+                }
+    }
+}
