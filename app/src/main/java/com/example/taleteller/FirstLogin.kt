e326d0eded82cf23e5cde0a8344a47cf0e83aa14
Jakub97 <jakubkosek1997@wp.pl>
2020-05-30 21:38:31 +0200

    image

--- app/src/main/java/com/example/taleteller/FirstLogin.kt
+++ app/src/main/java/com/example/taleteller/FirstLogin.kt
@@ -1,31 +1,83 @@
 package com.example.taleteller
 
+import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_first_login.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.list_item.*
 import java.util.*
 import kotlin.collections.HashMap
 
 class FirstLogin : AppCompatActivity() {
+    lateinit var filepath : Uri
     var username : EditText ? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_login)
         Log.d( "Problem","Problem")
 
-        saveUsername.setOnClickListener(){
+        saveUsername2.setOnClickListener{
             saveUsername()
+            button2.setOnClickListener{
+                startFileChooser()
+            }
+            button3.setOnClickListener(){
+                uploadFile()
+            }
+
             startActivity(Intent(this,MainPage::class.java))
         }
     }
+    fun startFileChooser() {
+        var i = Intent()
+        i.setType("img/*")
+        i.setAction(Intent.ACTION_GET_CONTENT)
+        startActivityForResult(Intent.createChooser(i,"Choose Picture"),111)
+    }
+    fun uploadFile(){
+        if(filepath!=null){
+            var pd = ProgressDialog(this)
+            pd.setTitle("Uploading")
+            pd.show()
 
+            var imageRef = FirebaseStorage.getInstance().reference.child("img/pic.jpg")
+            imageRef.putFile(filepath)
+                .addOnSuccessListener {p0 ->
+                    pd.dismiss()
+                    Toast.makeText(applicationContext,"File Uploaded",Toast.LENGTH_LONG).show()
+                }
+                .addOnFailureListener{p0 ->
+                    pd.dismiss()
+                    Toast.makeText(applicationContext,p0.message,Toast.LENGTH_LONG).show()
+                }
+                .addOnProgressListener {p0 ->
+                    var progress = (100.0 * p0.bytesTransferred) / p0.totalByteCount
+                    pd.setMessage("Uploaded ${progress.toInt()}%")
+
+                }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==111 && resultCode == Activity.RESULT_OK && data != null){
+            filepath = data.data!!
+            var bitmap = MediaStore.Images.Media.getBitmap(contentResolver,filepath)
+            imageView2.setImageBitmap(bitmap)
+
+        }
+    }
     fun saveUsername(){
         val db = FirebaseFirestore.getInstance()
         val u = FirebaseAuth.getInstance().currentUser
